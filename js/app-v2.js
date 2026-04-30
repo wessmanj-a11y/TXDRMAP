157ce83cbc5b65998bfa458b7c240745ba9e6e97
wessmanj-a11y <wessman.j@gmail.com>
2026-04-30 16:30:08 -0500

    Create app-v2.js

---
+++ js/app-v2.js
@@ -0,0 +1,27 @@
+async function loadAndRender(){
+  await loadData();
+  renderAll();
+}
+
+function renderAll(){
+  setButtonState();
+  renderMapLayers();
+  renderMetrics();
+  renderPanel();
+  renderTopPanels();
+  renderPointTable();
+  renderRoadTable();
+  renderOutageTrendChart();
+  renderHospitalTrendChart();
+}
+
+async function start(){
+  initMap();
+  renderControls();
+  await loadAndRender();
+}
+
+start().catch(err => {
+  console.error(err);
+  document.getElementById("sourceStatus").innerHTML = `Load failed: ${err.message}`;
+});
